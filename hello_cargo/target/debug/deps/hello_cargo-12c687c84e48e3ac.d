4fe3a5daaabcf403b7a5f9696870009bbd2aeec9
always_wsl <wnwngudwns@gmail.com>
2022-10-06 08:51:53 +0900

    hello_cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.d
@@ -0,0 +1,5 @@
+/home/joon/workspace/rust/hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.rmeta: src/main.rs
+
+/home/joon/workspace/rust/hello_cargo/target/debug/deps/hello_cargo-12c687c84e48e3ac.d: src/main.rs
+
+src/main.rs:
