58a8abd54a90ccc2d0e4659fb91199d3b3810981
always_wsl <wnwngudwns@gmail.com>
2022-10-06 08:53:29 +0900

    delete target

--- hello_cargo/target/debug/deps/hello_cargo-573f9d36bd6f9543.d
+++
@@ -1,5 +0,0 @@
-/home/joon/workspace/rust/hello_cargo/target/debug/deps/hello_cargo-573f9d36bd6f9543: src/main.rs
-
-/home/joon/workspace/rust/hello_cargo/target/debug/deps/hello_cargo-573f9d36bd6f9543.d: src/main.rs
-
-src/main.rs:
